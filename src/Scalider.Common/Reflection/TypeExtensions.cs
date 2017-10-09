cffa446a61760094f0019951018112ca7845d30b
Wilson Rivera <mr.wilson.rivera@gmail.com>
2017-10-09 09:11:10 -0400

    - added a package that provides "common" extension and utility classes.

---
+++ src/Scalider.Common/Reflection/TypeExtensions.cs
@@ -0,0 +1,43 @@
+﻿#region # using statements #
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace Scalider.Reflection
+{
+
+    /// <summary>
+    /// Provides extension methods for the <see cref="Type"/> class.
+    /// </summary>
+    public static class ReflectionExtensions
+    {
+
+        /// <summary>
+        /// Determines whether the <paramref name="this"/> is an anonymous
+        /// <see cref="Type"/>.
+        /// </summary>
+        /// <param name="this">The type to test.</param>
+        /// <returns>
+        /// true if the give <paramref name="this"/> is an anonymous
+        /// <see cref="Type"/>; otherwise, false.
+        /// </returns>
+        public static bool IsAnonymousType(this Type @this)
+        {
+            Check.NotNull(@this, nameof(@this));
+
+            // HACK: The only way to detect anonymous types right now.
+            var typeInfo = @this.GetTypeInfo();
+            return
+                typeInfo.IsDefined(typeof(CompilerGeneratedAttribute), false) &&
+                typeInfo.IsGenericType && @this.Name.Contains("AnonymousType") &&
+                (@this.Name.StartsWith("<>") || @this.Name.StartsWith("VB$")) &&
+                (typeInfo.Attributes & TypeAttributes.NotPublic) ==
+                TypeAttributes.NotPublic;
+        }
+
+    }
+
+}
---
+++ src/Scalider.Common/UriUtility.cs
@@ -0,0 +1,53 @@
+﻿#region # using statements #
+
+using System;
+using System.Text;
+using JetBrains.Annotations;
+
+#endregion
+
+namespace Scalider
+{
+
+    /// <summary>
+    /// Provides some utility methods for dealing with Uris and Uris related
+    /// operations.
+    /// </summary>
+    public static class UriUtility
+    {
+
+        /// <summary>
+        /// Transforms the string into a URL-friendly slug.
+        /// </summary>
+        /// <param name="str">The original string.</param>
+        /// <returns>
+        /// A string containing a url-friendly slug.
+        /// </returns>
+        [NotNull]
+        public static string ToFriendlyUri(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return string.Empty;
+
+            // Remove any special characters
+            var sb = new StringBuilder();
+            foreach (var chr in str.Trim().ToLower().RemoveDiacritics())
+            {
+                if (chr <= 'z' && chr >= 'a' || chr <= '9' && chr >= '0')
+                    sb.Append(chr);
+                else
+                    sb.Append('-');
+            }
+
+            // Remove double dashes
+            var output = sb.ToString().Normalize(NormalizationForm.FormC).Trim('-');
+            while (output.IndexOf("--", StringComparison.Ordinal) >= 0)
+                output = output.Replace("--", "-");
+
+            // Return generated result
+            return output;
+        }
+
+    }
+
+}
