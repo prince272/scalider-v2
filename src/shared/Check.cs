5b02c7df5944c3b728a0d8c937f77c9af10c62c9
Wilson Rivera <mr.wilson.rivera@gmail.com>
2017-10-26 06:37:22 -0400

    - added a few validators and comments to 'Check.cs'

--- src/shared/Check.cs
+++ src/shared/Check.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using JetBrains.Annotations;
 
 #endregion
@@ -16,6 +18,46 @@ namespace Scalider
     [DebuggerStepThrough]
     internal static class Check
     {
+
+        /// <summary>
+        /// Validates that <paramref name="value"/> is not NaN or infinity and throws
+        /// an exception when this condition is not met.
+        /// </summary>
+        /// <param name="value">The number to validate.</param>
+        /// <param name="paramName">The name of the argument to use to report as
+        /// null.</param>
+        /// <exception cref="ArgumentException">When <paramref name="value"/>
+        /// is NaN or infinity.</exception>
+        public static void IsValidNumber(float value,
+            [InvokerParameterName, NotNull] string paramName)
+        {
+            NameIsNotNullOrEmpty(paramName, nameof(paramName));
+            if (float.IsNaN(value) || float.IsInfinity(value))
+            {
+                throw new ArgumentException(
+                    "The value must be a valid finite number", paramName);
+            }
+        }
+
+        /// <summary>
+        /// Validates that <paramref name="value"/> is not NaN or infinity and throws
+        /// an exception when this condition is not met.
+        /// </summary>
+        /// <param name="value">The number to validate.</param>
+        /// <param name="paramName">The name of the argument to use to report as
+        /// null.</param>
+        /// <exception cref="ArgumentException">When <paramref name="value"/>
+        /// is NaN or infinity.</exception>
+        public static void IsValidNumber(double value,
+            [InvokerParameterName, NotNull] string paramName)
+        {
+            NameIsNotNullOrEmpty(paramName, nameof(paramName));
+            if (double.IsNaN(value) || double.IsInfinity(value))
+            {
+                throw new ArgumentNullException(
+                    "The value must be a valid finite number", paramName);
+            }
+        }
 
         /// <summary>
         /// Validates that <paramref name="value"/> is not null and throws an
@@ -36,6 +78,16 @@ namespace Scalider
                 throw new ArgumentNullException(paramName);
         }
 
+        /// <summary>
+        /// Validates that <paramref name="value"/> is not null or only contains
+        /// whitespaces and throws an exception if the condition is not met.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="paramName"></param>
+        /// <exception cref="ArgumentNullException">When <paramref name="value"/>
+        /// is null.</exception>
+        /// <exception cref="ArgumentException">When <paramref name="value"/> only
+        /// contains whitespaces.</exception>
         [ContractAnnotation("value:null => halt")]
         public static void NotNullOrEmpty(string value,
             [InvokerParameterName, NotNull] string paramName)
@@ -49,6 +101,19 @@ namespace Scalider
                 throw new ArgumentException(
                     $"The string parameter {paramName} cannot be null or " +
                     "contain only whitespaces.", paramName);
+            }
+        }
+
+        public static void IsValidFileName(string value,
+            [InvokerParameterName, NotNull] string paramName)
+        {
+            NotNullOrEmpty(value, nameof(value));
+            NameIsNotNullOrEmpty(paramName, nameof(paramName));
+
+            if (value.Intersect(Path.GetInvalidFileNameChars()).Any())
+            {
+                throw new ArgumentException(
+                    "The file name contains invalid characters", paramName);
             }
         }
 
