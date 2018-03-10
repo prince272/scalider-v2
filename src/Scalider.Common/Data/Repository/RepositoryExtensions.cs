72472d29b2c18fd736fe871fb10d5913ad20ad19
Wilson Rivera <mr.wilson.rivera@gmail.com>
2018-03-10 16:00:51 -0400

    - moved the repository pattern

---
+++ src/Scalider.Common/Data/Repository/RepositoryExtensions.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Scalider.Data.Entity;
+
+namespace Scalider.Data.Repository
+{
+
+    /// <summary>
+    /// Provides extension methods for the <see cref="IRepository"/> interfaces.
+    /// </summary>
+    public static class RepositoryExtensions
+    {
+
+        /// <summary>
+        /// Adds a collection of new entities to the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to add to the
+        /// data store.</param>
+        public static void AddRange<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] params TEntity[] entities)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            batchRepository.AddRange(entities.AsEnumerable());
+        }
+
+        /// <summary>
+        /// Asynchronously adds a collection of new entities to the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to add to the data store.</param>
+        /// <returns>
+        /// The <see cref="Task"/> object representing the asynchronous operation.
+        /// </returns>
+        public static Task AddRangeAsync<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] params TEntity[] entities)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            return batchRepository.AddRangeAsync(entities.AsEnumerable());
+        }
+
+        /// <summary>
+        /// Asynchronously adds a collection of new entities to the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to add to the data store.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken" /> to
+        /// observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// The <see cref="Task"/> object representing the asynchronous operation.
+        /// </returns>
+        public static Task AddRangeAsync<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] TEntity[] entities, CancellationToken cancellationToken)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            return batchRepository.AddRangeAsync(entities.AsEnumerable(), cancellationToken);
+        }
+
+        /// <summary>
+        /// Updates a collection of entities in the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to add to the data store.</param>
+        public static void UpdateRange<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] params TEntity[] entities)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            batchRepository.UpdateRange(entities);
+        }
+
+        /// <summary>
+        /// Asynchronously updates a collection of entities in the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to add to the data store.</param>
+        /// <returns>
+        /// The <see cref="Task"/> object representing the asynchronous operation.
+        /// </returns>
+        public static Task UpdateRangeAsync<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] params TEntity[] entities)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            return batchRepository.UpdateRangeAsync(entities);
+        }
+
+        /// <summary>
+        /// Asynchronously updates a collection of entities in the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to add to the data store.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken" /> to
+        /// observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// The <see cref="Task"/> object representing the asynchronous operation.
+        /// </returns>
+        public static Task UpdateRangeAsync<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] TEntity[] entities, CancellationToken cancellationToken)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            return batchRepository.UpdateRangeAsync(entities, cancellationToken);
+        }
+
+        /// <summary>
+        /// Removes a collection of entities from the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to remove from the data store.</param>
+        public static void RemoveRange<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] params TEntity[] entities)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            batchRepository.RemoveRange(entities);
+        }
+
+        /// <summary>
+        /// Asynchronously removes a collection of entities from the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to remove from the data store.</param>
+        /// <returns>
+        /// The <see cref="Task"/> object representing the asynchronous operation.
+        /// </returns>
+        public static Task RemoveRangeAsync<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] params TEntity[] entities)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            return batchRepository.UpdateRangeAsync(entities);
+        }
+
+        /// <summary>
+        /// Asynchronously removes a collection of entities from the data store.
+        /// </summary>
+        /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
+        /// <param name="repository">The <see cref="IRepository{TEntity}"/>.</param>
+        /// <param name="entities">The collection of entities to remove from the data store.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the
+        /// task to complete.</param>
+        /// <returns>
+        /// The <see cref="Task"/> object representing the asynchronous operation.
+        /// </returns>
+        public static Task RemoveRangeAsync<TEntity>([NotNull] this IRepository<TEntity> repository,
+            [NotNull] TEntity[] entities, CancellationToken cancellationToken)
+            where TEntity : class, IEntity
+        {
+            Check.NotNull(repository, nameof(repository));
+            Check.NotNull(entities, nameof(entities));
+
+            if (!(repository is IBatchRepository<TEntity> batchRepository))
+            {
+                // The type of the repository doesn't support
+                throw new ArgumentException();
+            }
+
+            return batchRepository.UpdateRangeAsync(entities, cancellationToken);
+        }
+
+    }
+
+}
--- src/Scalider.EntityFrameworkCore/Data/Repository/EfRepositoryOfTContextAndTEntity.cs
+++ src/Scalider.EntityFrameworkCore/Data/Repository/EfRepositoryOfTContextAndTEntity.cs
@@ -12,21 +12,19 @@ namespace Scalider.Data.Repository
 {
 
     /// <summary>
-    /// Provides an implementation of the <see cref="IRepository{TEntity}"/>
-    /// generic interface that uses Entity Framework Core to read and write on
-    /// the database.
+    /// Provides an implementation of the <see cref="IRepository{TEntity}"/> generic interface that uses
+    /// Entity Framework Core to read and write on the database.
     /// </summary>
     /// <typeparam name="TContext">The type encapsulating the database
     /// context.</typeparam>
     /// <typeparam name="TEntity">The type encapsulating the entity.</typeparam>
-    public class EfRepository<TContext, TEntity> : IEfRepository<TEntity>
+    public class EfRepository<TContext, TEntity> : IBatchRepository<TEntity>
         where TContext : DbContext
         where TEntity : class, IEntity
     {
 
         /// <summary>
-        /// Initializes a new instance of the
-        /// <see cref="EfRepository{TContext, TEntity}"/> class.
+        /// Initializes a new instance of the <see cref="EfRepository{TContext, TEntity}"/> class.
         /// </summary>
         /// <param name="context"></param>
         [SuppressMessage("ReSharper", "MemberCanBeProtected.Global")]
@@ -46,16 +44,15 @@ namespace Scalider.Data.Repository
         /// <summary>
         /// Gets the <see cref="DbSet{T}"/> being used by the repository.
         /// </summary>
-        protected Lazy<DbSet<TEntity>> DbSet =>
-            new Lazy<DbSet<TEntity>>(() => Context.Set<TEntity>());
+        [UsedImplicitly]
+        protected Lazy<DbSet<TEntity>> DbSet => new Lazy<DbSet<TEntity>>(() => Context.Set<TEntity>());
 
         /// <summary>
-        /// Returns a <see cref="IQueryable{T}"/> with all the navigations for
-        /// the entity of the repository, if the entity defines any navigation.
+        /// Returns a <see cref="IQueryable{T}"/> with all the navigations for the entity of the repository, if
+        /// the entity defines any navigation.
         /// </summary>
         /// <returns>
-        /// The <see cref="IQueryable{T}"/> with the included navigations
-        /// (if any).
+        /// The <see cref="IQueryable{T}"/> with the included navigations (if any).
         /// </returns>
         [SuppressMessage("ReSharper", "VirtualMemberNeverOverridden.Global")]
         protected virtual IQueryable<TEntity> GetQueryableWithIncludes()
@@ -85,8 +82,7 @@ namespace Scalider.Data.Repository
             GetQueryableWithIncludes().ToList();
 
         /// <inheritdoc />
-        public virtual async Task<IEnumerable<TEntity>> GetAllAsync(
-            CancellationToken cancellationToken = default) =>
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync( CancellationToken cancellationToken = default) =>
             await GetQueryableWithIncludes().ToListAsync(cancellationToken);
 
         /// <inheritdoc />
@@ -97,8 +93,7 @@ namespace Scalider.Data.Repository
         }
 
         /// <inheritdoc />
-        public virtual Task AddAsync(TEntity entity,
-            CancellationToken cancellationToken = default)
+        public virtual Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
         {
             Check.NotNull(entity, nameof(entity));
             return DbSet.Value.AddAsync(entity, cancellationToken);
@@ -112,8 +107,7 @@ namespace Scalider.Data.Repository
         }
 
         /// <inheritdoc />
-        public virtual Task UpdateAsync(TEntity entity,
-            CancellationToken cancellationToken = default)
+        public virtual Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
         {
             Check.NotNull(entity, nameof(entity));
             DbSet.Value.Update(entity);
@@ -129,8 +123,7 @@ namespace Scalider.Data.Repository
         }
 
         /// <inheritdoc />
-        public virtual Task RemoveAsync(TEntity entity,
-            CancellationToken cancellationToken = default)
+        public virtual Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default)
         {
             Check.NotNull(entity, nameof(entity));
             DbSet.Value.Remove(entity);
@@ -146,11 +139,42 @@ namespace Scalider.Data.Repository
         }
 
         /// <inheritdoc />
-        public Task AddRangeAsync(IEnumerable<TEntity> entities,
-            CancellationToken cancellationToken = default)
+        public virtual Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
         {
             Check.NotNull(entities, nameof(entities));
             return DbSet.Value.AddRangeAsync(entities, cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public virtual void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            Check.NotNull(entities, nameof(entities));
+            DbSet.Value.UpdateRange(entities);
+        }
+
+        /// <inheritdoc />
+        public virtual Task UpdateRangeAsync(IEnumerable<TEntity> entities,
+            CancellationToken cancellationToken = default)
+        {
+            Check.NotNull(entities, nameof(entities));
+            DbSet.Value.UpdateRange(entities);
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc />
+        public virtual void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            Check.NotNull(entities, nameof(entities));
+            DbSet.Value.RemoveRange(entities);
+        }
+
+        /// <inheritdoc />
+        public virtual Task RemoveRangeAsync(IEnumerable<TEntity> entities,
+            CancellationToken cancellationToken = default)
+        {
+            Check.NotNull(entities, nameof(entities));
+            DbSet.Value.RemoveRange(entities);
+            return Task.CompletedTask;
         }
 
         #endregion
