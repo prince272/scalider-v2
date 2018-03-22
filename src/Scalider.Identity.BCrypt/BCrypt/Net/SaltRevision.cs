02e8e67c06041f905216f67aad879b87ef893bdc
Wilson Rivera <mr.wilson.rivera@gmail.com>
2018-03-22 16:01:33 -0400

    - removed the package dependency for BCrypt - added the source for BCrypt directly to the project (https://github.com/neoKushan/BCrypt.Net-Core)

---
+++ src/Scalider.Identity.BCrypt/BCrypt/Net/SaltRevision.cs
@@ -0,0 +1,19 @@
+﻿#pragma warning disable 1591
+
+// ReSharper disable once CheckNamespace
+namespace BCrypt.Net
+{
+    
+    public enum SaltRevision
+    {
+
+        Revision2,
+        Revision2A,
+        Revision2B,
+        Revision2X,
+        Revision2Y
+
+    }
+    
+}
+#pragma warning restore 1591
