2fcb8ec783da130e6c2652b6a249182ac17d4f92
Wilson Rivera <mr.wilson.rivera@gmail.com>
2018-06-21 17:09:19 -0400

    - removed the BaseEntity class - updated the equality methods for the entity

--- src/Scalider.Core/Domain/Entity/BaseEntityOfTKey.cs
+++ src/Scalider.Core/Domain/Entity/BaseEntityOfTKey.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using JetBrains.Annotations;
 
@@ -12,7 +10,7 @@ namespace Scalider.Domain.Entity
     /// Provides a base class for implementations of the <see cref="IEntity{TKey}"/> generic interface.
     /// </summary>
     /// <typeparam name="TKey">The type encapsulating the identity of the entity.</typeparam>
-    public abstract class BaseEntity<TKey> : BaseEntity, IEntity<TKey>
+    public abstract class BaseEntity<TKey> : IEntity<TKey>
         where TKey : IEquatable<TKey>
     {
 
@@ -27,17 +25,19 @@ namespace Scalider.Domain.Entity
         public override string ToString() => $"{GetType().Name} {Id}";
 
         /// <inheritdoc />
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        protected sealed override bool Equals(BaseEntity other) =>
-            other is BaseEntity<TKey> entity && Equals(entity);
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            return obj.GetType() == GetType() && Equals((BaseEntity<TKey>)obj);
+        }
 
         /// <inheritdoc />
         [SuppressMessage("ReSharper", "NonReadonlyMemberInGetHashCode")]
-        public override int GetHashCode()
-        {
-            Debug.Assert(Id != null, nameof(Id) + " != null");
-            return EqualityComparer<TKey>.Default.GetHashCode(Id);
-        }
+        public override int GetHashCode() => EqualityComparer<TKey>.Default.GetHashCode(Id);
 
         /// <summary>
         /// Determines whether the specified object is equal to the current object.
@@ -46,14 +46,34 @@ namespace Scalider.Domain.Entity
         /// <returns>
         /// true if the specified object is equal to the current object; otherwise, false.
         /// </returns>
-        protected virtual bool Equals(BaseEntity<TKey> other)
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        protected bool Equals(BaseEntity<TKey> other)
         {
-            if (ReferenceEquals(null, other))
-                return false;
-            
-            return ReferenceEquals(this, other) ||
-                   EqualityComparer<TKey>.Default.Equals(Id, other.Id);
+            return other != null && EqualityComparer<TKey>.Default.Equals(Id, other.Id);
         }
+
+        /// <summary>
+        /// Indicates whether the values of two specified <see cref="BaseEntity{TKey}" /> have the same value.
+        /// </summary>
+        /// <param name="left">The first object to compare.</param>
+        /// <param name="right">The second object to compare.</param>
+        /// <returns>
+        /// <c>true</c> if the value of <paramref name="left" /> is the same as the value of <paramref name="right" />;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        public static bool operator ==(BaseEntity<TKey> left, BaseEntity<TKey> right) =>
+            left?.Equals(right) ?? Equals(right, null);
+
+        /// <summary>
+        /// Indicates whether the values of two specified <see cref="BaseEntity{TKey}" /> have different values.
+        /// </summary>
+        /// <param name="left">The first object to compare.</param>
+        /// <param name="right">The second object to compare.</param>
+        /// <returns>
+        /// <c>true</c> if the value of <paramref name="left" /> is different from the value of
+        /// <paramref name="right" />; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool operator !=(BaseEntity<TKey> left, BaseEntity<TKey> right) => !(left == right);
 
         #region IEntity<TKey> Members
 
